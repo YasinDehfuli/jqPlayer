772f3d5836966f1e5f5cfdcb817592c73f88463b
YasinDehfuli <ydehfuli@gmail.com>
2022-09-10 10:32:25 +0430

    add playlist

--- player.js
+++ player.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    var audio = $("#music")[0];
+    console.log($("#music"));
+    $("#player ul li").click(function () {
+        $("#music").attr('src', $(this).data('src'));
+        audio.play();
+        $("#player li").removeClass('active');
+        $(this).addClass('active');
+    });
 });
